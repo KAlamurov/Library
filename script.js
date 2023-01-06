02591aa4930a558dc22f19c49c5b64e1d3efbf00
kiko0o <kirilalamurov@gmail.com>
2023-01-06 19:28:24 +0200

    Push books to array via frontend and display

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-function Book(title,author,pages,read) {
+let myLibrary = [];
 
+function Book(title,author,pages,read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = () => {
-        return `${this.title} by ${this.author} , ${this.pages} pages, ${this.read}`;
-    }
 }
 
-const Harry = new Book("Harry Potter","JK Rowling", "320", "Not yet read");
-console.log(Harry.info());
+function addBookToLibrary() {
+
+   const title = document.getElementById('title').value;
+   const author = document.getElementById('author').value;
+   const pages = document.getElementById('pages').value;
+   const read = "Not yet";
+
+    const userBook = new Book(`${title}`, `${author}`, `${pages}`, `${read}`);
+   myLibrary.push(userBook);
+
+    const bookTable = document.getElementById('books');
+  
+        const row = bookTable.insertRow(-1);
+        const cell = row.insertCell(-1);
+        const cell2 = row.insertCell(-1);
+        const cell3 = row.insertCell(-1);
+        const cell4 = row.insertCell(-1);
+        cell.innerHTML = title;
+        cell2.innerHTML = author;
+        cell3.innerHTML = pages;
+        cell4.innerHTML = read;
+
+}
+
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+}
+
+const form = document.querySelector(".btn");
+
+form.addEventListener("click", btnclick, false);
+
+function btnclick(event) {
+    event.preventDefault();
+    closeForm();
+}
